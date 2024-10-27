677ad069df9a2ca4dd96d4aa05ab091f04c22955
Edmund Yu <38992135+Nolelle@users.noreply.github.com>
2024-10-27 11:46:12 -0600

    update app and todo list to use tasks list

--- App.jsx
+++ App.jsx
@@ -4,15 +4,19 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native';
 import TodoForm from "./ToDoForm";
 import TodoList from "./ToDoList";
 
 export default function App() {
+  const [tasks, setTasks] = useState(['Do laundry',
+  'Go to gym',
+  'Walk dog']);
+
   return (
     <SafeAreaView>
-      <TodoList/>
+      <TodoList tasks = {tasks}/>
       <TodoForm/>
     </SafeAreaView>
   );
