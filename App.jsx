983482148539829f95026d648d287d2d19a0ab88
Nolelle <eyu1@ualberta.ca>
2024-10-31 15:25:31 -0600

    finish lab4

--- App.jsx
+++ App.jsx
@@ -6,18 +6,22 @@
 
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native';
-import TodoForm from "./ToDoForm";
-import TodoList from "./ToDoList";
+import TodoForm from './ToDoForm';
+import TodoList from './ToDoList';
 
 export default function App() {
   const [tasks, setTasks] = useState(['Do laundry',
   'Go to gym',
   'Walk dog']);
 
+  const addTask = (task) =>{
+    setTasks([...tasks, task]);
+  };
+
   return (
     <SafeAreaView>
       <TodoList tasks = {tasks}/>
-      <TodoForm/>
+      <TodoForm addTask = {addTask}/>
     </SafeAreaView>
   );
 }
--- ToDoForm.jsx
+++ ToDoForm.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Button, StyleSheet, TextInput, View } from 'react-native';
 
-export default function ToDoForm() {
+export default function ToDoForm({addTask}) {
+
+  const [taskText, setTaskText] = useState('');
     return (
         <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="Add a new task..." autoFocus = {true}/>
-        <Button title="Add" />
+        <TextInput style={styles.input} placeholder="Add a new task..." onChangeText = {(text) => setTaskText(text)} value = {taskText} autoFocus = {true}/>
+        <Button title="Add Task" onPress= {() => addTask(taskText)} />
       </View>
-    )
+    );
 }
 
 
